834a27196d7da0f8fe28a19597399438f4944c39
sorcery <sorcery.zzz@gmail.com>
2015-06-02 20:27:03 +0800

    CRATE  MODEL TO PROJECT

---
+++ document/Blowing.MoveHouse/Blowing.MoveHouse.Model/MoveHouse/Base.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blowing.MoveHouse.Model.MoveHouse
+{
+   public class Base
+   {
+       #region - feild -
+       private DateTime _f_insertTime;
+       private DateTime _f_updateTime;
+       
+       #endregion
+
+       #region - property -
+       /// <summary>
+       /// 记录生成时间
+       /// </summary>
+       public DateTime F_InsetTime { set { _f_insertTime = value; } get { return _f_insertTime; } }
+       /// <summary>
+       /// 记录更新时间
+       /// </summary>
+       public DateTime F_UpdateTime { set { _f_updateTime = value; } get { return _f_updateTime; } }
+       
+       #endregion
+    }
+}
--- document/Blowing.MoveHouse/Blowing.MoveHouse.Model/MoveHouse/MoveHousePerson.cs
+++ document/Blowing.MoveHouse/Blowing.MoveHouse.Model/MoveHouse/MoveHousePerson.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Blowing.MoveHouse.Model.MoveHouse
 {
-   public  class MoveHousePerson
+    public class MoveHousePerson : Base
    {
        #region - feild -
        private string _f_bjp_id;
@@ -14,12 +14,31 @@ namespace Blowing.MoveHouse.Model.MoveHouse
        private int _f_bjpcartypeid;
        private decimal _f_bjpCostStart;
        private decimal _f_bjpCostEnd;
-       private DateTime _f_insertTime;
-
+  
        #endregion
 
        #region - property -
-       
+       /// <summary>
+       /// 主键
+       /// </summary>
+       public string F_Bjp_ID { set { _f_bjp_id = value; } get { return _f_bjp_id; } }
+      /// <summary>
+      /// 用户ID
+      /// </summary>
+       public string F_Bjp_UID { set { _f_bjp_uid = value; } get { return _f_bjp_uid; } }
+      /// <summary>
+      /// 车型
+      /// </summary>
+       public int F_BjpCarTypeID { set { _f_bjpcartypeid = value; } get {return  _f_bjpcartypeid; } }
+       /// <summary>
+       /// 最低费用
+       /// </summary>
+       public decimal F_BjpCostStart { set { _f_bjpCostStart = value; } get { return _f_bjpCostStart; } }
+      /// <summary>
+      /// 最高费用
+      /// </summary>
+       public decimal F_BjpCostEnd { set { _f_bjpCostEnd = value; } get { return _f_bjpCostEnd; } }
+ 
        #endregion
 
    }
