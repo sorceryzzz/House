19a67890206b99ace58069df5750c3b2782de172
pengbowen <pengbowen@mofangge.com>
2015-07-21 17:25:33 +0800

    新建解决方案和创建相关项目.

---
+++ document/Blowing.MoveHouse/Blowing.MoveHouse.Dal/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blowing.MoveHouse.Dal
+{
+    public class Class1
+    {
+    }
+}
